3434d26df8837bfcc03b651d9a4ed95f061054d3
Mikko Väisänen <mjvaisanen@gmail.com>
2017-04-19 10:58:58 +0300

    Initial landscape & brush castle

---
+++ Source/Outsider/Outsider.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __OUTSIDER_H__
+#define __OUTSIDER_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/Outsider/OutsiderGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "OutsiderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOutsiderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOutsiderGameMode();
+};
+
+
+
